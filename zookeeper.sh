1ecbcf1e3e3657dee139db3022eedb20aeb2e078
Artem Buria <artem_buria@epam.com>
2017-07-19 15:49:40 +0300

    agent-master relationship establishe

--- zookeeper.sh
+++ zookeeper.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 timedatectl set-timezone Europe/Kiev
-yum install java-1.8.0-openjdk -y
-cd /opt/ && curl -O http://apache.org/dist/zookeeper/current/zookeeper-3.4.10.tar.gz
-tar xvzf zookeeper-3.4.10.tar.gz
-useradd zookeeper
-chown -R zookeeper /opt/zookeeper-3.4.10
-ln -s /opt/zookeeper-3.4.10 /opt/zookeeper
-chown -h zookeeper /opt/zookeeper
-mkdir /var/lib/zookeeper
-chown zookeeper. /var/lib/zookeeper
-cd /opt/zookeeper/conf
-cp zoo_sample.cfg zoo.cfg
-
-cat > /etc/systemd/system/zookeeper.service << EOF
-[Unit]
-Description=Apache Zookeeper server
-Documentation=http://zookeeper.apache.org
-Requires=network.target remote-fs.target
-After=network.target remote-fs.target
-
-[Service]
-Type=forking
-User=zookeeper
-Group=zookeeper
-ExecStart=/opt/zookeeper/bin/zkServer.sh start
-ExecStop=/opt/zookeeper/bin/zkServer.sh stop
-ExecReload=/opt/zookeeper/bin/zkServer.sh restart
-WorkingDirectory=/var/lib/zookeeper
-
-[Install]
-WantedBy=multi-user.target
+rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum update
+yum -y install puppet-agent
+cat >> /etc/puppetlabs/puppet/puppet.conf << EOF
+[main]
+server = ${dns_name}
+environment = ${env}
 EOF
-systemctl start zookeeper
+cat >> /etc/hosts << EOF
+${puppet_ip} ${dns_name}
+EOF
+service puppet start
+#add  role
+cat >> /root/role.rb << EOF
+Facter.add(:role) do
+  setcode do
+    'zookeeper'
+  end
+end
+EOF
+echo "export FACTERLIB=/root/" >> /root/.bash_profile
