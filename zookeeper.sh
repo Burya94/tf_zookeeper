b0558c052797678af8410fa4d028923e20a30bb7
Artem Buria <artem_buria@epam.com>
2017-07-31 13:56:37 +0300

    provision fix

--- zookeeper.sh
+++ zookeeper.sh
@@ -5,6 +5,7 @@ yum update
 yum -y install puppet-agent
 yum -y install wget
 cd /opt && wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
+cd /opt && tar xzvf jdk-8u141-linux-x64.tar.gz
 cat >> /etc/puppetlabs/puppet/puppet.conf << EOF
 [main]
 server = ${dns_name}
@@ -24,6 +25,6 @@ end
 EOF
 catt >> /root/.bash_profile << EOF
 export FACTERLIB=/root/
-export JAVA_HOME=/opt/jdk1.8.0_141/
+export JAVA_HOME=/opt/jdk1.8.0_141
 export PATH=$PATH:$JAVA_HOME/bin
 EOF
