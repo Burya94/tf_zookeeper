e4a0feef29780b9be381c8fc32c0bc73df8bbe60
Artem Buria <artem_buria@epam.com>
2017-08-10 16:47:28 +0300

    try amazone linux

--- zookeeper.sh
+++ zookeeper.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-timedatectl set-timezone Europe/Kiev
-rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum update
-yum -y install puppet-agent
+rpm -Uvh https://yum.puppetlabs.com/el/5Client/PC1/x86_64/puppet-agent-1.9.3-1.el5.x86_64.rpm
+yum -y install puppet
 yum -y install wget
 cd /opt && wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
 cd /opt && tar xzvf jdk-8u141-linux-x64.tar.gz
 cat >> /etc/puppetlabs/puppet/puppet.conf << EOF
 [main]
 server = ${dns_name}
-environment = ${environment}
+environment = ${env}
 EOF
 cat >> /etc/hosts << EOF
 ${puppet_ip} ${dns_name}
@@ -28,3 +26,32 @@ export FACTERLIB=/root/
 export JAVA_HOME=/opt/jdk1.8.0_141/
 export PATH=$PATH:"$JAVA_HOME"bin/
 EOF
+#timedatectl set-timezone Europe/Kiev
+#rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+#yum update
+#yum -y install puppet-agent
+#yum -y install wget
+#cd /opt && wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz"
+#cd /opt && tar xzvf jdk-8u141-linux-x64.tar.gz
+#cat >> /etc/puppetlabs/puppet/puppet.conf << EOF
+#[main]
+#server = ${dns_name}
+#environment = ${environment}
+#EOF
+#cat >> /etc/hosts << EOF
+#${puppet_ip} ${dns_name}
+#EOF
+#service puppet start
+#add  role
+#cat >> /root/role.rb << EOF
+#Facter.add(:role) do
+#  setcode do
+#    'zookeeper'
+#  end
+#end
+#EOF
+#cat >> /root/.bash_profile << EOF
+#export FACTERLIB=/root/
+#export JAVA_HOME=/opt/jdk1.8.0_141/
+#export PATH=$PATH:"$JAVA_HOME"bin/
+#EOF
