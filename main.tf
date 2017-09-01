dcae0d68f48c869aa4d61a141c75353dd7786c94
Artem Buria <artem_buria@epam.com>
2017-09-01 12:43:49 +0300

    asg rebase

--- main.tf
+++ main.tf
@@ -22,19 +22,36 @@ data "template_file" "userdata" {
   }
 }
 
-resource "aws_instance" "zookeeper" {
-  count                = 3
-  key_name             = "${var.key_name}"
-  ami                  = "ami-a4c7edb2"#"${data.aws_ami.centos7.id}"
-  instance_type        = "${var.instype}"
-  user_data            = "${data.template_file.userdata.rendered}"
-  subnet_id            = "${element(var.subnet_id, count.index)}"
+resource "aws_autoscaling_group" "testscale" {
+  availability_zones        = ["us-east-1a"]
+  name                      = "terraasg_test"
+  launch_configuration      = "${aws_launch_configuration.zookeeper}"
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  min_size                  = 3
+  max_size                  = 3
+  desired_capacity          = 3
+  vpc_zone_identifier       = ["${var.subnet_id}"]
+
+  lifecycle {
+    create_before_destroy = "true"
+  }
+}
+
+resource "aws_launch_configuration" "zookeeper" {
+  #count                = 3
+  key_name      = "${var.key_name}"
+  ami           = "ami-a4c7edb2"                            #"${data.aws_ami.centos7.id}"
+  instance_type = "${var.instype}"
+  user_data     = "${data.template_file.userdata.rendered}"
+
+  #subnet_id            = "${element(var.subnet_id, count.index)}"
   security_groups      = ["${var.sec_group}"]
   iam_instance_profile = "${aws_iam_instance_profile.zoo_profile.name}"
   depends_on           = ["aws_iam_instance_profile.zoo_profile"]
 
-  tags {
-    Name = "${count.index}.Zookeeper Instance"
+  lifecycle {
+    create_before_destroy = "true"
   }
 }
 
@@ -46,18 +63,18 @@ data "aws_iam_policy_document" "s3_access" {
 
     resources = [
       "arn:aws:s3:::${var.s3_tfstate_bucket_name}/*",
-      "arn:aws:s3:::${var.s3_tfstate_bucket_name}"
+      "arn:aws:s3:::${var.s3_tfstate_bucket_name}",
     ]
   }
 
   statement {
-      actions =[
-          "kinesis:*",
-      ]
+    actions = [
+      "kinesis:*",
+    ]
 
-      resources =[
-          "arn:aws:kinesis:*:${var.account_id}:stream/${var.stream_name}",
-      ]
+    resources = [
+      "arn:aws:kinesis:*:${var.account_id}:stream/${var.stream_name}",
+    ]
   }
 }
 
